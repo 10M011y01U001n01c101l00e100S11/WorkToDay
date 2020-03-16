fe33842d8acda9523499c8928af60f91fb379979
Warayut Taekrathok <warayut2555@gmail.com>
2020-03-17 01:04:19 +0700

    update

---
+++ src/components/CheckIn.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import WorkIcon from '@material-ui/icons/Work';
+import moment from "moment";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+export default function CheckIn({CheckInData}) {
+    const classes = useStyles();
+  document.title = 'Work To Day | Users';
+    return (
+        <List className={classes.root}>
+            {CheckInData?.reverse().map(({ _key, check_in, check_out, check_user, work_list }) => (
+
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <WorkIcon />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={work_list} secondary={'Check In: ' + moment(check_in).format('LL HH:mm') } />
+                </ListItem>
+            ))}
+        </List>
+    );
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as FirestoreService from '../services/RealtimeDatabase';
 import { Button, Container } from "@material-ui/core";
+import CheckIn from "./CheckIn";
 
 export default function Home() {
     document.title = 'Work To Day';
+    const [userCheckData, setCheckUserData] = useState([])
     const onCheckIn = () => {
         FirestoreService.setCheckIn()
     }
+    FirestoreService.fetchIP().then(e => console.log(e))
+
+    useEffect(() => {
+        FirestoreService.getCheckInUsers().on("value", snapshot => {
+            const array = [];
+            // For each data in the entry
+            snapshot.forEach(el => {
+                // Push the object to the array
+                // If you also need to store the unique key from firebase,
+                // You can use array.push({ ...el.val(), key: el.key });
+                array.push(el.val());
+            });
+            setCheckUserData(array);
+        });
+    }, [])
+
+    console.log(userCheckData);
+    
     return (
         <Container>
             <Button
@@ -18,6 +38,7 @@ export default function Home() {
             >
                 Check In
             </Button>
+            <CheckIn CheckInData={userCheckData} />
         </Container>
     );
 }
