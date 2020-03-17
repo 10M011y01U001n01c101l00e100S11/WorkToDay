2ff9c4041aa724c1ad265eaf64b80f23cb5a4fa1
Warayut Taekrathok <warayut2555@gmail.com>
2020-03-17 13:45:33 +0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import * as FirestoreService from '../services/RealtimeDatabase';
 import { Button, Container } from "@material-ui/core";
 import CheckIn from "./CheckIn";
+import moment from "moment";
+import { useHistory } from "react-router-dom";
 
 export default function Home() {
     document.title = 'Work To Day';
     const [userCheckData, setCheckUserData] = useState([])
+    const history = useHistory();
     const onCheckIn = () => {
         FirestoreService.setCheckIn()
     }
@@ -28,33 +31,47 @@ export default function Home() {
         });
     }, [])
 
-    console.log(userCheckData?.[0]?.check_out);
+    console.log(moment().diff(moment(userCheckData?.[0]?.check_in), 'second'));
+
 
     return (
         <>
-        <br/>
+            <br />
             <Container>
                 <CheckIn CheckInData={userCheckData.reverse()} />
             </Container>
-            {userCheckData?.[0]?.check_in && !userCheckData?.[0]?.check_out ? <Button
-                type="button"
-                fullWidth
-                variant="contained"
-                color="primary"
-                onClick={onCheckOut}
-                style={{bottom: 0, position: 'fixed'}}
-            >
-                Check Out
-            </Button> : <Button
-                type="button"
-                fullWidth
-                variant="contained"
-                color="primary"
-                onClick={onCheckIn}
-                style={{bottom: 0, position: 'fixed'}}
-            >
-                Check In
-            </Button>}
+            {userCheckData?.[0]?.check_in && moment().diff(moment(userCheckData?.[0]?.check_in), 'second') < 10 ?
+                <Button
+                    type="button"
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => history.go()}
+                    style={{ bottom: 0, position: 'fixed' }}
+                >
+                    Warnning
+                </Button> :
+                userCheckData?.[0]?.check_in && !userCheckData?.[0]?.check_out ? <Button
+                    type="button"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    onClick={onCheckOut}
+                    style={{ bottom: 0, position: 'fixed' }}
+                >
+                    Check Out
+                </Button> : <Button
+                        type="button"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        onClick={onCheckIn}
+                        style={{ bottom: 0, position: 'fixed' }}
+                    >
+                        Check In
+                </Button>
+            }
+
         </>
     );
 }
