b71fd042bfdaadb36f50666f406ce1f148857d7e
Warayut Taekrathok <warayut2555@gmail.com>
2020-03-20 23:31:41 +0700

    update

--- src/services/RealtimeDatabase.jsx
+++ src/services/RealtimeDatabase.jsx
@@ -31,22 +31,27 @@ export const getCheckInUsers = (user, pass) => {
     return db.ref('tb_check').child(localStorage.getItem('login__key'))
 };
 
-export const setCheckIn = (user = localStorage.getItem('login_username'), pass = localStorage.getItem('login_password')) => {
-        let newKey = db.ref().child(`tb_check/${localStorage.getItem('login__key')}`).push().key;
-        let updates = {};
-        const list = {
-            "_key": newKey,
-            "check_in": moment().format(),
-            "check_out": "",
-            "check_user": user,
-            "work_list": moment().format('HH') >= 6 && moment().format('HH') <= 18 ? "Morning job" : "Late night",
-            "MAC_ADDRESS": 'mac'
-        }
-        updates[`/tb_check/${localStorage.getItem('login__key')}/` + newKey] = list;        
-        return db.ref().update(updates)
+export const setCheckIn = async (user = localStorage.getItem('login_username'), pass = localStorage.getItem('login_password')) => {
+    let newKey = await db.ref().child(`tb_check/${localStorage.getItem('login__key')}`).push().key;
+    let updates = await {};
+    let mac_address = await '';
+    let ip_address = await '';
+    await fetchMacAddress().then(async e => mac_address = await e)
+    await fetchIP().then(async e => ip_address = await e)
+    const list = await {
+        "_key": newKey,
+        "check_in": moment().format(),
+        "check_out": "",
+        "check_user": user,
+        "work_list": moment().format('HH') >= 6 && moment().format('HH') <= 18 ? "Morning job" : "Late night",
+        "MAC_ADDRESS": mac_address || '0',
+        "IP_ADDRESS": ip_address || '0',
+    }
+    updates[`/tb_check/${localStorage.getItem('login__key')}/` + newKey] = await list;
+    return db.ref().update(updates)
 };
 
-export const setCheckOut = (e) => {    
+export const setCheckOut = (e) => {
     let updates = {};
     const list = {
         "_key": e._key,
@@ -55,7 +60,7 @@ export const setCheckOut = (e) => {
         "check_user": e.check_user,
         "work_list": e.work_list
     }
-    updates[`/tb_check/${localStorage.getItem('login__key')}/` + e._key ] = list;
+    updates[`/tb_check/${localStorage.getItem('login__key')}/` + e._key] = list;
     return db.ref().update(updates)
 };
 
@@ -77,7 +82,18 @@ export const addUsers = (username = '', password = '', lastname = '', img = '', 
 
 
 export const fetchIP = () => {
-   return fetch('https://api.ipify.org/?format=json').then((e) => e.json()).then(e => (e.ip))
+    return fetch('https://api.ipify.org/?format=json').then((e) => e.json()).then(e => (e.ip))
+}
+
+export const fetchMacAddress = () => {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    return fetch("https://work-at-home.herokuapp.com/ ", requestOptions)
+        .then(response => response.text())
+        .then(result => result)
+        .catch(error => error);
 }
 
 export const IP_ADDRESS = () => {
