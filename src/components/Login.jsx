414b68c53f3c1dd8022aaef2a4ad1f19f7b42c32
nattavee <devilzeros@outlook.co.th>
2020-03-23 10:38:27 +0700

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -142,18 +142,6 @@ export default function Login() {
                         >
                             Sign In
                         </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link onClick={() => history.push('addusers')} href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link onClick={() => history.push('addusers')} variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
                         <Box mt={5}>
                             <Copyright />
                         </Box>
